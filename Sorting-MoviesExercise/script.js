31458411d38179b950ea1f0f25d4b83667abb13b
WTheodoro <theodoro.walison@gmail.com>
2021-03-29 18:05:48 -0300

    sort by rank

--- Sorting-MoviesExercise/script.js
+++ Sorting-MoviesExercise/script.js
@@ -61,7 +61,7 @@ let movies = [
 window.onload = function() {
 
     // Display Movies list
-    displayMovies(movies);
+    displayMovies(sortMoviesByRank(movies));
 }
 
 /**
@@ -89,26 +89,26 @@ function displayMovies(movies){
  * HINT: make sure you are comparing the right value in in if(...)
  * HINT: replace numbers with movies .
  */
-function sortMoviesByRank(numbers){
+function sortMoviesByRank(movies){
   // Code from previous sortBestRatingsFirst() function
-  for (let j = 0; j < numbers.length - 1; j++) {
+  for (let j = 0; j < movies.length - 1; j++) {
 
-      let max_num = numbers[j];
+      let max_rank = movies[j].rank;
       let max_location = j;
 
-      for (let i = j; i < numbers.length; i++) {
-          if (numbers[i] > max_num) {
+      for (let i = j; i < movies.length; i++) {
+          if (movies[i].rank > max_rank) {
               // Know max AND it's index (location)
-              max_num = numbers[i]
+              max_rank = movies[i].rank
               max_location = i
           }
       }
       // swap the first and the last
-      numbers[max_location] = numbers[j] // --> 10
-      numbers[j] = max_num
+      movies[max_location] = movies[j] // --> 10
+      movies[j].rank = max_rank
   }
 
-  return numbers
+  return movies
 }
 
 /**
